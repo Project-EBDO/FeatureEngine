943c0a72ee4db3c4526c2b54804c404d8689a6c8
Alex2242 <alex@degurse.fr>
2018-04-05 18:43:12 +0200

    Change parameters of the test that raises exception

--- src/test/scala/org/ode/engine/signal_processing/TestFFT.scala
+++ src/test/scala/org/ode/engine/signal_processing/TestFFT.scala
@@ -230,7 +230,7 @@ class TestFFT extends FlatSpec with Matchers {
 
   "FFT" should "raise IllegalArgumentException when given a signal of the wrong length" in {
     val signal: Array[Double] = (0.0 to 10.0 by 0.1).map(cos).toArray
-    val fftClass: FFT = new FFT(10 * signal.length)
+    val fftClass: FFT = new FFT(10)
 
     an [IllegalArgumentException] should be thrownBy fftClass.compute(signal)
   }

dc644901688170ed056cd140a6ae59b473979629
Joseph Allemandou <joseph.allemandou@gmail.com>
2018-06-18 16:56:44 +0200

    Big refactor (comments, perf tests and other)
    This commit is the perfect example of what we should try to avoid.
    It contains many different fixes all over the place.
     - Comments normalization (javadoc style instead of scaladoc style)
       so that scalastyle-indentation can be enabled fail.
     - Class parameters indentation when on multiple lines (for same
       reason as above)
     - Update some comments, names and others for better understanding
     - Update some signal-processing code to use performance best-pratices
     - Add performance-test framework to help us on those tasks
     - Completely rework performance tests to fit in the new framework

--- src/test/scala/org/ode/engine/perf/TestFFTOneSided.scala
+++ src/test/scala/org/ode/engine/perf/TestFFTOneSided.scala
@@ -1,30 +1,29 @@
 /** Copyright (C) 2017-2018 Project-ODE
-  *
-  * This program is free software: you can redistribute it and/or modify
-  * it under the terms of the GNU General Public License as published by
-  * the Free Software Foundation, either version 3 of the License, or
-  * (at your option) any later version.
-  *
-  * This program is distributed in the hope that it will be useful,
-  * but WITHOUT ANY WARRANTY; without even the implied warranty of
-  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  * GNU General Public License for more details.
-  *
-  * You should have received a copy of the GNU General Public License
-  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
-  */
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 package org.ode.engine.perf
-
 
 import org.ode.engine.signal_processing.{FFTTwoSided, FFT}
 import org.scalatest.{FlatSpec, Matchers}
 
-/**
-  * Tests for one-sided FFT
-  * Author: Alexandre Degurse, Jospeh Allemandou&(
-  */
 
+/**
+ * Tests for one-sided FFT
+ * Authors: Alexandre Degurse, Jospeh Allemandou
+ */
 class TestFFTOneSided extends FlatSpec with Matchers {
 
   "FFTOneSided" should "compute a fft faster than the two-sided version" in {

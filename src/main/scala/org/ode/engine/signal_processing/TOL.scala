ca1354112742863810eb5a7646a8ae02631791f1
Alex2242 <alex@degurse.fr>
2018-06-19 18:25:17 +0200

    Add TOL class

---
+++ src/main/scala/org/ode/engine/signal_processing/TOL.scala
@@ -0,0 +1,155 @@
+/** Copyright (C) 2017-2018 Project-ODE
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.ode.engine.signal_processing
+
+import scala.math.{log10, pow, log, floor}
+/**
+ * Class that computes Third-Octave Level bands over a PSD.
+ *
+ * Author: Paul Nguyen HD, Alexandre Degurse
+ *
+ * @param nfft The number of points of the two-sided spectrum that this class should handle
+ * @param samplingRate The sampling rate of the signal
+ * @param lowFreq The low boundary of the frequency range to study
+ * @param highFreq The high boundary of the frequency range to study
+ */
+
+
+class TOL
+(
+  val nfft: Int,
+  val samplingRate: Double,
+  val lowFreq: Option[Double] = None,
+  val highFreq: Option[Double] = None
+) {
+
+  if (nfft < samplingRate) {
+    throw new IllegalArgumentException(
+      s"Incorrect window size ($nfft) for TOL (samplingRate)"
+    )
+  }
+
+  if (
+    lowFreq.isDefined && (lowFreq.get < 25.0 || lowFreq.get > highFreq.getOrElse(samplingRate/2.0))
+  ) {
+    throw new IllegalArgumentException(
+      s"Incorrect low frequency (${lowFreq.get}) for TOL (25.0)"
+    )
+  }
+
+  if (
+    highFreq.isDefined
+    && (highFreq.get > samplingRate/2.0 || highFreq.get < lowFreq.getOrElse(25.0))
+  ) {
+    throw new IllegalArgumentException(
+      s"Incorrect high frequency (${highFreq.get}) for TOL (${samplingRate/2.0})"
+    )
+  }
+
+  private val expectedWelchSize = if (nfft % 2 == 0) nfft/2 + 1 else (nfft + 1)/2
+
+  // Generate third octave band boundaries for each center frequency of third octave
+  private val bounds: Array[(Double, Double)] = {
+    // we're using acoustic constants
+    // scalastyle:off magic.number
+    val lowCut: Double = if (lowFreq.isDefined) lowFreq.get else 25.0
+    val highCut: Double = if (highFreq.isDefined) highFreq.get else samplingRate / 2.0
+
+    // Frequency Key from ANSI standard
+    val keyFreq = 1000.0
+
+    val firstCompleteTOIdx: Int = math.ceil(10.0 * math.log10(lowCut)).toInt
+    val lastCompleteTOIdx: Int = math.floor(10.0 * math.log10(highCut)).toInt
+
+    val keepFirstPartial: Boolean = lowCut > (25.0 * pow(10, 0.05))
+    val keepLastPartial: Boolean =
+      highCut > (pow(10, 0.05) * pow(10, lastCompleteTOIdx.toDouble/10.0))
+
+
+    val tobCenterFreq =
+      Range(
+        firstCompleteTOIdx + (if (keepFirstPartial) -1 else 0),
+        lastCompleteTOIdx + (if (keepLastPartial) 1 else 0)
+      )
+        .map(i => math.pow(10, i.toDouble/10.0))
+        .toArray
+
+    tobCenterFreq.map(freq => (freq*pow(10,-0.05), freq*pow(10,0.05))).toArray
+    // scalastyle:on magic.number
+  }
+
+  /**
+   * Function used to associate a frequency of a spectrum to an index of a discrete
+   * nfft-sized spectrum
+   *
+   * @param freq The frequency to be matched with an index
+   * @return The index that corresponds to the given frequency
+   */
+  def frequencyToIndex(freq: Double): Int = (freq * nfft / samplingRate).toInt
+
+  // Compute the indices associated with each TOB boundary
+  private val boundIndicies: Array[(Int, Int)] = bounds.map(
+    bound => (frequencyToIndex(bound._1), frequencyToIndex(bound._2))
+  )
+
+  /**
+   * Function that computes the Third Octave Levels over a PSD
+   *
+   * @param psd The one-sided PSD as an Array[Double] of length expectedPSDSize
+   * @param vADC The voltage of Analog Digital Converter used in the microphone,
+   * or in other words ADC peak voltage (e.g 1.414 V)
+   * @param microSensitivity Microphone sensitivity (without gain)
+   * (-170 dB re 1 V/Pa to -140 dB re 1 V/Pa is a range often used)
+   * @param gain Gain used for the hydrophones (range from 20 dB to 25 dB)
+   * @return The Third Octave Levels over the PSD as a Array[Double]
+   */
+  def compute
+  (
+    psd: Array[Double],
+    vADC: Double = 1.0,
+    microSensitivity: Double = 0.0,
+    gain: Double = 0.0
+  ): Array[Double] = {
+
+    if (psd.length != expectedWelchSize) {
+      throw new IllegalArgumentException(
+        s"Incorrect PSD size (${psd.length}) for TOL ($expectedWelchSize)"
+      )
+    }
+
+    val logNormalization: Double = microSensitivity + gain + 20*log10(1.0/vADC)
+    val tols = new Array[Double](boundIndicies.length)
+
+    // scalastyle:off while var.local
+    var i = 0
+    var j = 0
+    var tol: Double = 0.0
+
+    while (i < boundIndicies.length) {
+      j = boundIndicies(i)._1
+      while (j < boundIndicies(i)._2) {
+        tol += psd(j)
+        j += 1
+      }
+      tols(i) = 10.0 * math.log10(tol) - logNormalization
+      tol = 0.0
+      i += 1
+    }
+    // scalastyle:on while var.local
+    tols
+  }
+}
---
+++ src/test/scala/org/ode/engine/signal_processing/TestTOL.scala
@@ -0,0 +1,171 @@
+/** Copyright (C) 2017 Project-ODE
+  *
+  * This program is free software: you can redistribute it and/or modify
+  * it under the terms of the GNU General Public License as published by
+  * the Free Software Foundation, either version 3 of the License, or
+  * (at your option) any later version.
+  *
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+  *
+  * You should have received a copy of the GNU General Public License
+  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+  */
+
+package org.ode.engine.signal_processing
+
+import org.ode.utils.test.ErrorMetrics.rmse;
+import org.scalatest.{FlatSpec, Matchers}
+
+/**
+  * Tests for TOL class
+  * Author: Alexandre Degurse
+  */
+
+
+class TestTOL extends FlatSpec with Matchers {
+
+  private val maxRMSE = 2.0E-15
+
+  private val psd128 = Array(
+    4.1602500000000000e+03, 8.3018982314637901e+02, 2.0767253111595070e+02,
+    9.2391640473272219e+01, 5.2043434459908696e+01, 3.3368095319152424e+01,
+    2.3223590267237746e+01, 1.7106902885582432e+01, 1.3137071184544086e+01,
+    1.0415505025764748e+01, 8.4689253698497744e+00, 7.0288200600409816e+00,
+    5.9336480124593187e+00, 5.0814952804032201e+00, 4.4054892166379576e+00,
+    3.8602754069873511e+00, 3.4142135623730945e+00, 3.0446864506055240e+00,
+    2.7351795158150223e+00, 2.4734065785976029e+00, 2.2500743071156775e+00,
+    2.0580482561886715e+00, 1.8917779545958380e+00, 1.7468929996990219e+00,
+    1.6199144044217739e+00, 1.5080450889126371e+00, 1.4090156545183423e+00,
+    1.3209693768284887e+00, 1.2423754209351645e+00, 1.1719626332156958e+00,
+    1.1086685176760165e+00, 1.0515995439451622e+00, 1.0000000000000000e+00,
+    9.5322735079138210e-01, 9.1073259539451312e-01, 8.7204449718226218e-01,
+    8.3675683885799723e-01, 8.0451805762327444e-01, 7.7502276639280554e-01,
+    7.4800477949818067e-01, 7.2323134608584516e-01, 7.0049835876883393e-01,
+    6.7962635431497331e-01, 6.6045716107122188e-01, 6.4285107722770385e-01,
+    6.2668448697361989e-01, 6.1184783961696443e-01, 5.9824393097252226e-01,
+    5.8578643762690530e-01, 5.7439866371671622e-01, 5.6401246710270447e-01,
+    5.5456733767016053e-01, 5.4600960522778752e-01, 5.3829175834238596e-01,
+    5.3137185861294145e-01, 5.2521303749535786e-01, 5.1978306494829041e-01,
+    5.1505398096938670e-01, 5.1100178259948836e-01, 5.0760616024666172e-01,
+    5.0485027826763218e-01, 5.0272059567891592e-01, 5.0120672368413632e-01,
+    5.0030131742372386e-01, 2.5000000000000000e-01
+  )
+
+  it should "compute Third Octave Band Boundaries when studying default frequency range" in {
+    val nfft = 128
+    val samplingRate = 128.0
+
+    val tolClass = new TOL(nfft, samplingRate)
+
+    val expectedBoudaries: Array[(Double, Double)] = Array(
+      (22.38721138568339 , 28.18382931264453),
+      (28.183829312644537, 35.48133892335754),
+      (35.481338923357555, 44.66835921509632),
+      (44.66835921509631 , 56.2341325190349)
+    )
+
+    tolClass.bounds.length should be(expectedBoudaries.length)
+
+    val zippedResults: Array[(Array[Double], Array[Double])] =
+      tolClass.bounds.zip(expectedBoudaries)
+      .map(bound => (bound._1.productIterator.toArray.map(_.asInstanceOf[Double]),
+        bound._2.productIterator.toArray.map(_.asInstanceOf[Double])))
+
+
+    zippedResults.foreach(
+        bound => rmse(bound._1, bound._2) should be < maxRMSE
+      )
+  }
+
+  it should "compute Third Octabe Levels when studying default frequency range" in {
+    val nfft = 128
+    val samplingRate = 128.0
+
+    val tolClass = new TOL(nfft, samplingRate)
+
+    val tols = tolClass.compute(psd128)
+
+    val expectedTols = Array(
+      9.775688536361923, 8.714892243362911, 8.325191414850961, 8.388036763027877
+    )
+
+    rmse(tols, expectedTols) should be < maxRMSE
+  }
+
+  it should "compute Third Octave Band Boundaries when studying custom frequency range" in {
+    val nfft = 128
+    val samplingRate = 128.0
+    val lowFreq = Some(35.2)
+    val highFreq = Some(61.9)
+
+    val tolClass = new TOL(nfft, samplingRate, lowFreq, highFreq)
+
+    val expectedBoudaries: Array[(Double, Double)] = Array(
+      (28.183829312644537, 35.48133892335754),
+      (35.481338923357555, 44.66835921509632),
+      (44.66835921509631 , 56.2341325190349)
+    )
+
+    tolClass.bounds.length should be(expectedBoudaries.length)
+
+    val zippedResults: Array[(Array[Double], Array[Double])] =
+      tolClass.bounds.zip(expectedBoudaries)
+      .map(bound => (bound._1.productIterator.toArray.map(_.asInstanceOf[Double]),
+        bound._2.productIterator.toArray.map(_.asInstanceOf[Double])))
+
+
+    zippedResults.foreach(
+        bound => rmse(bound._1, bound._2) should be < maxRMSE
+      )
+  }
+
+  it should "compute Third Octabe Levels when studying custom frequency range" in {
+    val nfft = 128
+    val samplingRate = 128.0
+    val lowFreq = Some(35.2)
+    val highFreq = Some(61.9)
+
+    val tolClass = new TOL(nfft, samplingRate, lowFreq, highFreq)
+
+    val tols = tolClass.compute(psd128)
+
+    val expectedTols = Array(
+      8.714892243362911, 8.325191414850961, 8.388036763027877
+    )
+
+    rmse(tols, expectedTols) should be < maxRMSE
+  }
+
+  it should "raise IllegalArgumentException when given a mishaped PSD" in {
+    val tolClass = new TOL(100, 100.0)
+
+    an [IllegalArgumentException] should be thrownBy tolClass.compute(Array(1.0))
+  }
+
+  it should "raise IllegalArgumentException when given windows that are smaller than 1 second" in {
+    an [IllegalArgumentException] should be thrownBy new TOL(100, 1000.0)
+  }
+
+  it should "raise IllegalArgumentException when given low frequency is higher than sampling rate / 2" in {
+    an [IllegalArgumentException] should be thrownBy new TOL(100, 100.0, Some(200.0))
+  }
+
+  it should "raise IllegalArgumentException when given high frequency is higher than sampling rate / 2" in {
+    an [IllegalArgumentException] should be thrownBy new TOL(100, 100.0,  Some(100.0))
+  }
+
+  it should "raise IllegalArgumentException when given high frequency is smaller than 25 Hz" in {
+    an [IllegalArgumentException] should be thrownBy new TOL(100, 100.0, Some(25.0), Some(0.0))
+  }
+
+  it should "raise IllegalArgumentException when given low frequency is smaller than 25 Hz" in {
+    an [IllegalArgumentException] should be thrownBy new TOL(100, 100.0,  Some(0.0))
+  }
+
+  it should "raise IllegalArgumentException when given low frequency is higher than high frequency" in {
+    an [IllegalArgumentException] should be thrownBy new TOL(100, 100.0,  Some(40.0), Some(30.0))
+  }
+}

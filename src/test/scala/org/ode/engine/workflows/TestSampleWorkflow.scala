831af440f6780f863f59d828baceb093a1d04a87
Alex2242 <alex@degurse.fr>
2018-06-21 10:29:05 +0200

    Add rmse for Record and AggregatedRecord in ErrorMetrics

--- src/test/scala/org/ode/engine/workflows/TestSampleWorkflow.scala
+++ src/test/scala/org/ode/engine/workflows/TestSampleWorkflow.scala
@@ -1,18 +1,18 @@
 /** Copyright (C) 2017-2018 Project-ODE
-  *
-  * This program is free software: you can redistribute it and/or modify
-  * it under the terms of the GNU General Public License as published by
-  * the Free Software Foundation, either version 3 of the License, or
-  * (at your option) any later version.
-  *
-  * This program is distributed in the hope that it will be useful,
-  * but WITHOUT ANY WARRANTY; without even the implied warranty of
-  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  * GNU General Public License for more details.
-  *
-  * You should have received a copy of the GNU General Public License
-  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
-  */
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 package org.ode.engine.workflows
 
@@ -45,81 +45,6 @@ class TestSampleWorkflow
 
   private type Record = (Float, Array[Array[Array[Double]]])
   private type AggregatedRecord = (Float, Array[Array[Double]])
-
-  def compareResultRecord(
-    recordsA: Array[Record],
-    recordsB: Array[Record]
-  ): Unit = {
-
-    recordsA.length should equal(recordsB.length)
-
-    val numRecords = recordsA.length
-    val numChannels = recordsA(0)._2.length
-    val numSegments = recordsA(0)._2(0).length
-    val segmentLength = recordsA(0)._2(0)(0).length
-
-    var r = 0
-    var c = 0
-    var s = 0
-
-    while (r < numRecords) {
-      // records keys should be equal
-      recordsA(r)._1 should equal(recordsB(r)._1)
-
-      // records should have the same number of channels
-      recordsA(r)._2.length should equal(numChannels)
-      recordsB(r)._2.length should equal(numChannels)
-
-      while (c < numChannels) {
-        // each record should have the same number of segments
-        recordsA(r)._2(c).length should equal(numSegments)
-        recordsB(r)._2(c).length should equal(numSegments)
-
-        var s = 0
-        while (s < numSegments) {
-          // segments should have the same length
-          recordsA(r)._2(c)(s).length should equal(segmentLength)
-          recordsB(r)._2(c)(s).length should equal(segmentLength)
-
-          // finally compare values
-          ErrorMetrics.rmse(recordsA(r)._2(c)(s), recordsB(r)._2(c)(s)) should be < maxRMSE
-          s += 1
-        }
-        c += 1
-      }
-      r += 1
-    }
-  }
-  def compareResultAggregatedRecord(
-    recordsA: Array[AggregatedRecord],
-    recordsB: Array[AggregatedRecord]
-  ): Unit = {
-    val numChannels = recordsA(0)._2.length
-    val recordLength = recordsA(0)._2(0).length
-
-    recordsA
-      .zip(recordsB)
-      .foreach{case (recA, recB) =>
-        // records keys should be equal
-        // fails, spark first record key doesn't start at 0.0
-        // recA._1 should equal(recB._1)
-
-        // records should have the same number of channels
-        recA._2.length should equal(numChannels)
-        recB._2.length should equal(numChannels)
-
-        var c = 0
-        while (c < numChannels) {
-          // each record should have the same number of segments
-          recA._2(c).length should equal(recordLength)
-          recB._2(c).length should equal(recordLength)
-
-          // finally compare values
-          ErrorMetrics.rmse(recA._2(c), recB._2(c)) should be < maxRMSE
-          c += 1
-        }
-      }
-  }
 
   "SampleWorkflow" should "generate results of expected size" in {
 
@@ -248,9 +173,9 @@ class TestSampleWorkflow
     val scalaWelchs = resultMapScala("welchs").right.get
     val scalaSPLs = resultMapScala("spls").right.get
 
-    compareResultRecord(scalaFFT, sparkFFT)
-    compareResultRecord(scalaPeriodograms, sparkPeriodograms)
-    compareResultAggregatedRecord(scalaWelchs, sparkWelchs)
-    compareResultAggregatedRecord(scalaSPLs, sparkSPLs)
+    ErrorMetrics.rmse(scalaFFT, sparkFFT) should be < maxRMSE
+    ErrorMetrics.rmse(scalaPeriodograms, sparkPeriodograms) should be < maxRMSE
+    ErrorMetrics.rmse(scalaWelchs, sparkWelchs) should be < maxRMSE
+    ErrorMetrics.rmse(scalaSPLs, sparkSPLs) should be < maxRMSE
   }
 }

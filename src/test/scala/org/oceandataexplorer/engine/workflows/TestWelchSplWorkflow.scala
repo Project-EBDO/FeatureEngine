ab4f13d1c051bd3f9dc420aac93b5e016172cdc9
Alex2242 <alex@degurse.fr>
2018-12-11 12:46:30 +0100

    Split SampleWorkflow into TolWorkflow & WelchSplWorkflow

--- src/test/scala/org/oceandataexplorer/engine/workflows/TestWelchSplWorkflow.scala
+++ src/test/scala/org/oceandataexplorer/engine/workflows/TestWelchSplWorkflow.scala
@@ -16,6 +16,8 @@
 
 package org.oceandataexplorer.engine.workflows
 
+import org.oceandataexplorer.engine.io.HadoopWavReader
+import org.oceandataexplorer.engine.signalprocessing.SoundCalibration
 
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SparkSession
@@ -28,13 +30,13 @@ import org.oceandataexplorer.utils.test.OdeCustomMatchers
 
 
 /**
- * Tests for [[SampleWorkflow]] class
- * It uses [[ScalaSampleWorkflow]] as reference to test [[SampleWorkflow]] results.
+ * Tests for [[WelchSplWorkflow]] class
+ * It uses [[ScalaSampleWorkflow]] as reference to test [[WelchSplWorkflow]] results.
  *
  * @author Alexandre Degurse, Joseph Allemandou
  */
 
-class TestSampleWorkflow extends FlatSpec
+class TestWelchSplWorkflow extends FlatSpec
   with Matchers with SharedSparkContext with OdeCustomMatchers {
 
   /**
@@ -42,7 +44,7 @@ class TestSampleWorkflow extends FlatSpec
    */
   val maxRMSE = 1.0E-16
 
-  "SampleWorkflow" should "generate results of expected size" in {
+  "WelchSplWorkflow" should "generate results of expected size" in {
 
     val spark = SparkSession.builder.getOrCreate
 
@@ -60,8 +62,24 @@ class TestSampleWorkflow extends FlatSpec
     val soundSampleSizeInBits = 16
     val soundDurationInSecs = 2.5f
 
+    val hadoopWavReader = new HadoopWavReader(
+      spark,
+      recordSizeInSec
+    )
 
-    val sampleWorkflow = new SampleWorkflow(
+    val records = hadoopWavReader.readWavRecords(
+      soundUri.toString,
+      soundsNameAndStartDate,
+      soundSamplingRate,
+      soundChannels,
+      soundSampleSizeInBits
+    )
+
+    val calibrationClass = SoundCalibration(0.0)
+
+    val calibratedRecords: RDD[Record] = records.mapValues(chan => chan.map(calibrationClass.compute))
+
+    val sampleWorkflow = new WelchSplWorkflow(
       spark,
       recordSizeInSec,
       windowSize,
@@ -70,11 +88,8 @@ class TestSampleWorkflow extends FlatSpec
     )
 
     val results = sampleWorkflow(
-      soundUri.toString,
-      soundsNameAndStartDate,
-      soundSamplingRate,
-      soundChannels,
-      soundSampleSizeInBits
+      calibratedRecords,
+      soundSamplingRate
     )
 
     val expectedRecordNumber = (soundDurationInSecs / recordSizeInSec).toInt
@@ -117,8 +132,24 @@ class TestSampleWorkflow extends FlatSpec
     val soundStartDate = "1978-04-11T13:14:20.200Z"
     val soundsNameAndStartDate = List(("sin_16kHz_2.5s.wav", new DateTime(soundStartDate)))
 
+   val hadoopWavReader = new HadoopWavReader(
+      spark,
+      recordSizeInSec
+    )
 
-    val sampleWorkflow = new SampleWorkflow(
+    val records = hadoopWavReader.readWavRecords(
+      soundUri.toString,
+      soundsNameAndStartDate,
+      soundSamplingRate,
+      soundChannels,
+      soundSampleSizeInBits
+    )
+
+    val calibrationClass = SoundCalibration(0.0)
+
+    val calibratedRecords: RDD[Record] = records.mapValues(chan => chan.map(calibrationClass.compute))
+
+    val sampleWorkflow = new WelchSplWorkflow(
       spark,
       recordSizeInSec,
       windowSize,
@@ -127,11 +158,8 @@ class TestSampleWorkflow extends FlatSpec
     )
 
     val sparkResults = sampleWorkflow(
-      soundUri.toString,
-      soundsNameAndStartDate,
-      soundSamplingRate,
-      soundChannels,
-      soundSampleSizeInBits
+      calibratedRecords,
+      soundSamplingRate
     )
 
     val sparkTs: Array[Long] = sparkResults
@@ -181,106 +209,6 @@ class TestSampleWorkflow extends FlatSpec
 
     welchs should rmseMatch(scalaWelchs)
     spls should rmseMatch(scalaSPLs)
-  }
-
-  it should "generate the same results as the pure scala workflow with TOL" in {
-    val spark = SparkSession.builder.getOrCreate
-
-    // Signal processing parameters
-    val recordSizeInSec = 1.0f
-    val soundSamplingRate = 16000.0f
-    val windowSize = 512
-    val windowOverlap = 128
-    val nfft = 512
-    val lowFreqTOL = Some(3000.0)
-    val highFreqTOL = Some(7000.0)
-
-    // Sound parameters
-    val soundUri = getClass.getResource("/wav/sin_16kHz_2.5s.wav").toURI
-    val soundChannels = 1
-    val soundSampleSizeInBits = 16
-    val soundStartDate = "1978-04-11T13:14:20.200Z"
-    val soundsNameAndStartDate = List(("sin_16kHz_2.5s.wav", new DateTime(soundStartDate)))
-
-
-    val sampleWorkflow = new SampleWorkflow(
-      spark,
-      recordSizeInSec,
-      windowSize,
-      windowOverlap,
-      nfft,
-      lowFreqTOL,
-      highFreqTOL
-    )
-
-    val sparkResults = sampleWorkflow(
-      soundUri.toString,
-      soundsNameAndStartDate,
-      soundSamplingRate,
-      soundChannels,
-      soundSampleSizeInBits
-    )
-
-    val sparkTs: Array[Long] = sparkResults
-      .select("timestamp")
-      .collect()
-      .map{channels =>
-        val javaTs = channels.getTimestamp(0)
-        new DateTime(javaTs).instant.millis
-      }
-
-    val sparkWelchs: Array[Array[Array[Double]]] = sparkResults
-      .select("welch")
-      .collect()
-      .map{channels =>
-        channels.getSeq(0).asInstanceOf[Seq[Seq[Double]]]
-          .map(_.toArray).toArray
-      }
-
-    val sparkSPLs: Array[Array[Array[Double]]] = sparkResults
-      .select("spl")
-      .collect()
-      .map{channels =>
-        channels.getSeq(0).asInstanceOf[Seq[Seq[Double]]]
-          .map(_.toArray).toArray
-      }
-
-    val sparkTOLs: Array[Array[Array[Double]]] = sparkResults
-      .select("tol")
-      .collect()
-      .map{channels =>
-        channels.getSeq(0).asInstanceOf[Seq[Seq[Double]]]
-          .map(_.toArray).toArray
-      }
-
-    val welchs = sparkTs.zip(sparkWelchs)
-    val spls = sparkTs.zip(sparkSPLs)
-    val tols = sparkTs.zip(sparkTOLs)
-
-    val scalaWorkflow = new ScalaSampleWorkflow(
-      recordSizeInSec,
-      windowSize,
-      windowOverlap,
-      nfft,
-      lowFreqTOL,
-      highFreqTOL
-    )
-
-    val resultsScala = scalaWorkflow(
-      soundUri,
-      soundSamplingRate,
-      soundChannels,
-      soundSampleSizeInBits,
-      soundStartDate
-    )
-
-    val scalaWelchs = resultsScala("welch").right.get
-    val scalaSPLs = resultsScala("spl").right.get
-    val scalaTOLs = resultsScala("tol").right.get
-
-    welchs should rmseMatch(scalaWelchs)
-    spls should rmseMatch(scalaSPLs)
-    tols should rmseMatch(scalaTOLs)
   }
 
   it should "generate the results with the right timestamps" in {
@@ -302,8 +230,24 @@ class TestSampleWorkflow extends FlatSpec
     val soundStartDate = "1978-04-11T13:14:20.200Z"
     val soundsNameAndStartDate = List(("sin_16kHz_2.5s.wav", new DateTime(soundStartDate, DateTimeZone.UTC)))
 
+    val hadoopWavReader = new HadoopWavReader(
+      spark,
+      recordSizeInSec
+    )
 
-    val sampleWorkflow = new SampleWorkflow(
+    val records = hadoopWavReader.readWavRecords(
+      soundUri.toString,
+      soundsNameAndStartDate,
+      soundSamplingRate,
+      soundChannels,
+      soundSampleSizeInBits
+    )
+
+    val calibrationClass = SoundCalibration(0.0)
+
+    val calibratedRecords: RDD[Record] = records.mapValues(chan => chan.map(calibrationClass.compute))
+
+    val sampleWorkflow = new WelchSplWorkflow(
       spark,
       recordSizeInSec,
       windowSize,
@@ -312,11 +256,8 @@ class TestSampleWorkflow extends FlatSpec
     )
 
     val results = sampleWorkflow(
-      soundUri.toString,
-      soundsNameAndStartDate,
-      soundSamplingRate,
-      soundChannels,
-      soundSampleSizeInBits
+      calibratedRecords,
+      soundSamplingRate
     )
 
     val timestampsSpark = results.select("timestamp").collect()
@@ -331,36 +272,5 @@ class TestSampleWorkflow extends FlatSpec
 
     duration shouldEqual 1000
     lastRecordStartDate shouldEqual expectedLastRecordDate
-  }
-
-  it should "raise an IllegalArgumentException when trying to compute TOL on with recordDuration < 1.0 sec" in {
-    val spark = SparkSession.builder.getOrCreate
-
-    // Signal processing parameters
-    val recordSizeInSec = 0.1f
-    val soundSamplingRate = 100.0f
-    val windowSize = 10
-    val windowOverlap = 5
-    val nfft = 10
-    val lowFreqTOL = Some(20.0)
-    val highFreqTOL = Some(40.0)
-
-    val sampleWorkflow = new SampleWorkflow(
-      spark,
-      recordSizeInSec,
-      windowSize,
-      windowOverlap,
-      nfft,
-      lowFreqTOL,
-      highFreqTOL
-    )
-
-    val records: RDD[Record] = spark.sparkContext.parallelize(
-      Seq((0L, Array((1.0 to 100.0 by 1.0).toArray)))
-    )
-
-    the[IllegalArgumentException] thrownBy {
-      sampleWorkflow.computeTol(records, soundSamplingRate)
-    } should have message "Incorrect recordDurationInSec (0.1) for TOL computation"
   }
 }
